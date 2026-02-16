c86b45e801c0c34c09da6945380cb20b42a240ee
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-02-17 01:09:13 +0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   To-Do List/.gitignore
    	new file:   To-Do List/README.md
    	new file:   To-Do List/eslint.config.js
    	new file:   To-Do List/index.html
    	new file:   To-Do List/package-lock.json
    	new file:   To-Do List/package.json
    	new file:   To-Do List/public/vite.svg
    	new file:   To-Do List/src/App.css
    	new file:   To-Do List/src/App.tsx
    	new file:   To-Do List/src/assets/react.svg
    	new file:   To-Do List/src/components/InputField.tsx
    	new file:   To-Do List/src/components/TodoList.tsx
    	new file:   To-Do List/src/index.css
    	new file:   To-Do List/src/main.tsx
    	new file:   To-Do List/tsconfig.app.json
    	new file:   To-Do List/tsconfig.json
    	new file:   To-Do List/tsconfig.node.json
    	new file:   To-Do List/vite.config.ts

---
+++ To-Do List/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import InputField from "./components/InputField";
+import type { todos } from "./components/InputField";
+import TodoList from "./components/todoList";
+
+const App = () => {
+  const [title, settitle] = useState("")
+  const [details, setdetails] = useState("")
+  const [todos, settodos] = useState<todos[]>([])
+      
+  return (
+    <div className="app">
+      <InputField title={title} settitle={settitle} details={details} setdetails={setdetails} todos={todos} settodos={settodos} />
+      <TodoList todos={todos} />
+    </div>
+  )
+}
+
+export default App
---
+++ To-Do List/src/components/InputField.tsx
@@ -0,0 +1,57 @@
+export type todos = {
+  title: string;
+  details: string;
+}
+
+interface Props {
+  title: string;
+  settitle: React.Dispatch<React.SetStateAction<string>>;
+  details: string;
+  setdetails: React.Dispatch<React.SetStateAction<string>>;
+  todos: todos[];
+  settodos: React.Dispatch<React.SetStateAction<todos[]>>;
+}
+
+const InputField = ({title, settitle, details, setdetails, todos, settodos}: Props) => {
+  
+
+  function submitForm(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+
+    const newTodo = {
+      title: title,
+      details: details
+    }
+
+    if(title === "" || details === "") {
+      console.error("Please fill in all the fields");
+      return;
+    }
+    else{
+      const updatedTodos = [...todos, newTodo];
+      settodos(updatedTodos);
+    }
+    settitle("");
+    setdetails("");
+
+    console.log(todos);
+  }
+
+  return (
+    <>
+      <form className="form">
+        <p id="heading">Title: </p>
+        <input type="text" placeholder='Enter Title' className="input" value={title} onChange={(event)=>{
+          settitle(event.target.value)
+        }} />
+        <p id="heading">Details: </p>
+        <textarea placeholder='Enter Details' className="input" value={details} onChange={(event)=>{
+          setdetails(event.target.value)
+        }} />
+        <button type='submit' className='submit-button' onClick={(e)=>{submitForm(e)}}>Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default InputField
