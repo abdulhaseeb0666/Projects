f3018b131ca81e2e9f0638eb068ef926dc9585f8
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-02-19 00:23:12 +0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   To-Do List/src/components/DeleteTodo.tsx

--- To-Do List/src/components/DeleteTodo.tsx
+++ To-Do List/src/components/DeleteTodo.tsx
@@ -3,14 +3,14 @@ import type { todos } from './InputField';
 const DeleteTodo = (todos: todos[], settodos: React.Dispatch<React.SetStateAction<todos[]>>, index: number) => {
 
   function deleteTodo() {
-    const updatedTodos = [...todos];
+    const updatedTodos: todos[] = [...todos];
     updatedTodos.splice(index, 1);
     localStorage.setItem("todos", JSON.stringify(updatedTodos));
     settodos(updatedTodos);
   }
 
   return <button onClick={deleteTodo}>Delete</button>
-  
+
 }
 
 export default DeleteTodo
