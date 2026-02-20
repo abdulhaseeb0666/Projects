863231b3f5dc66feb164446daece8cc2eb7c18f1
Abdul Haseeb <abdulhaseebah7886786@gmail.com>
2026-02-20 12:06:36 +0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   Ecommerce App/package-lock.json
    	modified:   Ecommerce App/package.json
    	modified:   Ecommerce App/src/components/Navbar.tsx
    	modified:   Ecommerce App/src/components/Navbar2.tsx
    	deleted:    Ecommerce App/src/pages/Product.tsx

--- Ecommerce App/src/components/Navbar.tsx
+++ Ecommerce App/src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="h-25 flex items-center justify-between p-4 bg-gray-800 text-white">
         <div className="flex align-center items-center gap-5">
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPE9VD7hC5m5iUcMPpHnJWJj37QdoVWgzLsg&s" alt="Logo" className="h-20 w-20 bg-gray-800 rounded-full " />
-          <h1 className="text-2xl font-bold justify-self-start-start ">Ecommerce App</h1>
+          <h1 className="text-2xl font-bold justify-self-start-start ">Ecommerce App Project</h1>
         </div>
         <div className="flex space-x-4 gap-5">
             <Link to="/" className="hover:text-gray-400 font-bold">Home</Link>
--- Ecommerce App/src/components/Navbar2.tsx
+++ Ecommerce App/src/components/Navbar2.tsx
@@ -5,17 +5,18 @@ const Navbar2 = () => {
   return (
     <div className="h-10 px-5 bg-orange-500 text-white text-sm flex items-center justify-between">
         
-        {categories.map((category) => (
-          <li className="relative group text-center font-bold cursor-pointer list-none hover:text-gray-700">
-            <span>{category.main}</span>
-            <ul className="w-fit whitespace-nowrap absolute bg-gray-600 opacity-0 invisible top-full left-0 flex gap-2 rounded-xl shadow-amber-900 p-3 flex-col group-hover:opacity-100 group-hover:visible  ">
-              {category.sub.map((subItem) => (
-              <li className="text-center font-bold cursor-pointer list-none text-white hover:text-blue-400">{subItem}</li>
-              ))}
-            </ul>
-          </li>
-        ))
-        }
+      {categories.map((category, index) => (
+
+        <li key={category.main} className="relative group text-center font-bold cursor-pointer list-none hover:text-gray-700">
+          <span>{category.main}</span>
+          <ul className={`w-fit whitespace-nowrap absolute bg-gray-600 opacity-0 invisible top-full flex gap-2 rounded-xl shadow-amber-900 p-3 flex-col group-hover:opacity-100 group-hover:visible ${index === categories.length - 1 ? "left-[-50%]" : "left-0"}  `}>
+            {category.sub.map((subItem) => (
+            <li key={subItem} className="text-center font-bold cursor-pointer list-none text-white hover:text-blue-400">{subItem}</li>
+            ))}
+          </ul>
+        </li>
+
+      ))}
     
     </div>
   )
